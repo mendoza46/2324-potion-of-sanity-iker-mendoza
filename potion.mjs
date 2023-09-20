638ae5681201ab8f6c23e71d32ac0fdb47e93433
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-09-20 11:46:58 +0200

    [NEW] class Potion

--- potion.mjs
+++ potion.mjs
@@ -1,3 +1,23 @@
 export class Potion {
-    
+    constructor(name, value, weight, time) {
+        this.name   = name;
+        this.value  = value;
+        this.weight = weight;
+        this.time   = time;
+    }
+
+    static with(effect, weight, value) {
+        const type = effect.type === 'beneficial' ? "Potion" : "Poison";
+        const potion_name = `${type} of ${effect.name}`;
+        const time = 10;
+        return new Potion(potion_name, value, weight, time);
+    }
+
+    static failed() {
+        return new FailedPotion();
+    }
+
+    static sanity() {
+        return new PotionOfSanity();
+    }
 }
