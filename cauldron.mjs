d4a5c946ec7c0937b16e4a751a211d134fdd32ef
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-09-20 11:41:27 +0200

    [NEW] crear funcion createPotion

---
+++ cauldron.mjs
@@ -0,0 +1,33 @@
+import { Ingredient } from "./ingredient.mjs";
+import { Potion } from "./potion.mjs";
+
+class Cauldron {
+    constructor(ingredients){
+        ingredients = Ingredient;
+    }
+}
+
+const createPotion = (ingredient_name1, ingredient_name2) => {
+    const ingredient1 = this.ingredients.find(ingredient_name1);
+    const ingredient2 = this.ingredients.find(ingredient_name2);
+
+    const common_effects = ingredient1.findCommonEffects(ingredient2);
+
+    if(common_effects.length === 0)
+        return Potion.failed();
+
+        if(isPotionOfSanity(ingredient1, ingredient2)) {
+            return Potion.sanity();
+        } else {
+            return Potion.with(
+                common_effects[0],
+                ingredient1.weight + ingredient2.weight,
+                ingredient1.value + ingredient2.value
+            );
+        }
+}
+
+export{
+    Cauldron,
+    createPotion
+}
--- index.mjs
+++ index.mjs
@@ -1,14 +1,19 @@
 import {getData} from "./service.mjs";
 import { Ingredients } from "./ingredients.mjs";
+import { Cauldron } from "./cauldron.mjs";
 
 const execute = async () => {
     try{
         const data = await getData();
 
-        showIngredients(data);
+        // showIngredients(data);
         // console.log(data);
         //Creamos los ingredientes
         const ingredients = Ingredients.load(data);
+
+        //Creamos el caldero de pociones
+        const cauldron = new Cauldron(ingredients);
+        console.log(cauldron)
 
     } catch{
         //ERROR
